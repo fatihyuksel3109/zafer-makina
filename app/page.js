ecbe6eb3716b9e87d037877aaab3dc5a17e77c85
Fatih Yuksel <fatihyuksel3109@gmail.com>
2025-08-08 14:10:49 +0300

    fix hero message

--- app/page.js
+++ app/page.js
@@ -7,15 +7,34 @@ export default function ComingSoonPage() {
     <div
       className="min-h-screen flex items-center justify-center"
       style={{
-        background: 'linear-gradient(to bottom, #081323, #040815)',
+        background: 'linear-gradient(to bottom, #0A1B2A, #1C1A1A)',
       }}
     >
-      <div className="w-full max-w-7xl aspect-[4/3] px-4 py-12">
+      <div className="h-fit sm:w-full md:aspect-[4/3] sm:px-4 py-20 lg:aspect-[16/9]">
+
+        {/* Görsel Bileşenleri */}
+        {/* Mobile (9:16) */}
         <Image
-          src="/images/zafermakina-yakinda.jpg"
-          alt="Zafer Makina Yakında"
+          src="/images/zafermakina-small.png"
+          alt="Zafer Makina Yakında - İş makinaları yedek parça çözümlerimizle çok yakında yayındayız. - Mobil"
           fill
-          className="object-contain"
+          className="object-contain block md:hidden"
+          priority
+        />
+        {/* Tablet (4:3) */}
+        <Image
+          src="/images/zafermakina-large.png"
+          alt="Zafer Makina Yakında - İş makinaları yedek parça çözümlerimizle çok yakında yayındayız. - Tablet"
+          fill
+          className="object-contain hidden md:block lg:hidden"
+          priority
+        />
+        {/* Masaüstü (16:9) */}
+        <Image
+          src="/images/zafermakina-xlarge.png"
+          alt="Zafer Makina Yakında - İş makinaları yedek parça çözümlerimizle çok yakında yayındayız. - Masaüstü"
+          fill
+          className="object-contain hidden lg:block"
           priority
         />
       </div>
